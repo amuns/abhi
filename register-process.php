3fb37ed518940e16efbe96f9cdd5192c40c1bb34
amuns <amunpote@gmail.com>
2023-03-22 02:17:02 +0545

    Newly Updated

--- register-process.php
+++ register-process.php
@@ -13,25 +13,26 @@
 
    
     if($_SERVER['REQUEST_METHOD'] == "POST"){
-        if(isset($_POST, $_POST['fname'], $_POST['lname'], $_POST['age'], $_POST['address'], $_POST['email'])){
+        if(isset($_POST, $_POST['fname'], $_POST['lname'], $_POST['age'], $_POST['address'], $_POST['email'], $_SESSION['lastInsertId'])){
             $fname = validate($_POST['fname']);
             $lname = validate($_POST['lname']);
             $age = validate($_POST['age']);
             $addr = validate($_POST['address']);
             $email = validate($_POST['email']);
             $pass = validate($_POST['password']);
-
+            $id = $_SESSION['lastInsertId'];
             try{
-                $stmt=$conn->prepare("INSERT into patient_details(fname, lname, age, address, email) VALUES(:fname, :lname, :age, :addr, :email)");
+                $stmt=$conn->prepare("UPDATE patient_details SET fname=:fname, lname=:lname, age=:age, address=:addr, email=:email WHERE id=:id");
                 $stmt->execute([
                     "fname"=>$fname, 
                     "lname"=>$lname, 
                     "age"=>$age,
                     "addr"=>$addr,
-                    "email"=>$email
+                    "email"=>$email,
+                    "id"=>$id
                 ]);
-                $_SESSION['status'] = 0;
-                header("location: status.php");
+                $_SESSION['success'] = "Your data has been registered successfully!";
+                header("location: login.php");
                 exit;
             }catch(Exception $e){
                 $_SESSION['error'] = $e;
